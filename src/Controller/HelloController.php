43e01acbd6f21974056be23e9ad4e6c7619f45fc
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-03-19 15:01:20 +0100

    pre exercice

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -12,6 +12,7 @@ class HelloController extends AbstractController
     #[Route('/hello/{name?World}', name: 'app_hello', requirements: ['name' => '(?:\pL|[- ])+'])]
     public function index(string $name, #[Autowire('%app.version%')] string $sfVersion): Response
     {
+        dump($sfVersion);
         return $this->render('hello/index.html.twig', [
             'controller_name' => $name,
         ]);
